9568972753a72ef9b04a15d3a7365c2ce6ead3dd
pavel_bortnik <pavel_bortnik@epam.com>
2019-03-25 19:56:46 +0300

    Initial saucelabs extension point

---
+++ src/main/java/com/epam/reportportal/extension/saucelabs/SaucelabsExtension.java
@@ -0,0 +1,41 @@
+package com.epam.reportportal.extension.saucelabs;
+
+import com.epam.reportportal.extension.bugtracking.BtsExtension;
+import com.epam.ta.reportportal.entity.integration.Integration;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+
+/**
+ * @author <a href="mailto:pavel_bortnik@epam.com">Pavel Bortnik</a>
+ */
+public interface SaucelabsExtension extends BtsExtension {
+
+	/**
+	 * Downloads job video
+	 *
+	 * @param system Integration parameters
+	 * @param jobId  Saucelabs job id
+	 * @return Video input stream
+	 */
+	InputStream downloadVideo(Integration system, String jobId);
+
+	/**
+	 * Detailed information about Saucelabs steps
+	 *
+	 * @param system Integration parameters
+	 * @param jobId  Saucelabs job id
+	 * @return Json object that contains steps
+	 */
+	JSONObject getLogs(Integration system, String jobId);
+
+	/**
+	 * Returns detailed information about the job
+	 *
+	 * @param system Integration parameters
+	 * @param jobId  Saucelabs job id
+	 * @return Json object that contains job information
+	 */
+	JSONObject getJobInfo(Integration system, String jobId);
+
+}
