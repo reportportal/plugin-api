f56ab1fa1183d4197483bccd57572b43dcc92d1c
pavel_bortnik <pavel_bortnik@epam.com>
2019-03-25 20:27:07 +0300

    fix saucelabs extension point

--- src/main/java/com/epam/reportportal/extension/saucelabs/SaucelabsExtensionPoint.java
+++ src/main/java/com/epam/reportportal/extension/saucelabs/SaucelabsExtensionPoint.java
@@ -3,13 +3,14 @@ package com.epam.reportportal.extension.saucelabs;
 import com.epam.reportportal.extension.bugtracking.BtsExtension;
 import com.epam.ta.reportportal.entity.integration.Integration;
 import org.json.JSONObject;
+import org.pf4j.ExtensionPoint;
 
 import java.io.InputStream;
 
 /**
  * @author <a href="mailto:pavel_bortnik@epam.com">Pavel Bortnik</a>
  */
-public interface SaucelabsExtension extends BtsExtension {
+public interface SaucelabsExtensionPoint extends ExtensionPoint {
 
 	/**
 	 * Downloads job video
