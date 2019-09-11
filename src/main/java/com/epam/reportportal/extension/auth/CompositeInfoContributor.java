d9c8a40fd2356208360c42b4e2986251b7621aa7
yumfriez <budaevqwerty@gmail.com>
2019-09-11 18:12:13 +0300

    Info contributor added

---
+++ src/main/java/com/epam/reportportal/extension/auth/CompositeInfoContributor.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2019 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.reportportal.extension.auth;
+
+import com.google.common.collect.Lists;
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @author <a href="mailto:ivan_budayeu@epam.com">Ivan Budayeu</a>
+ */
+@Component
+public class CompositeInfoContributor implements InfoContributor {
+
+	private final List<ReportPortalInfoContributor> reportPortalInfoContributors = Lists.newArrayList();
+
+	@Override
+	public void contribute(Info.Builder builder) {
+		reportPortalInfoContributors.forEach(infoContributor -> infoContributor.contribute(builder));
+	}
+
+	public List<ReportPortalInfoContributor> getReportPortalInfoContributors() {
+		return reportPortalInfoContributors;
+	}
+
+	public void addInfoContributor(ReportPortalInfoContributor infoContributor) {
+		reportPortalInfoContributors.add(infoContributor);
+	}
+}
