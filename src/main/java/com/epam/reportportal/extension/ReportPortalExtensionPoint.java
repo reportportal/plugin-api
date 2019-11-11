25d7d29583bbeb5ac753514a99f60feb8ae37fd9
pavel_bortnik <pavel_bortnik@epam.com>
2019-11-11 22:13:45 +0300

    add allowedCommands constans

--- src/main/java/com/epam/reportportal/extension/ReportPortalExtensionPoint.java
+++ src/main/java/com/epam/reportportal/extension/ReportPortalExtensionPoint.java
@@ -2,13 +2,18 @@ package com.epam.reportportal.extension;
 
 import org.pf4j.ExtensionPoint;
 
-import java.util.List;
 import java.util.Map;
 
 /**
  * @author <a href="mailto:pavel_bortnik@epam.com">Pavel Bortnik</a>
  */
 public interface ReportPortalExtensionPoint extends ExtensionPoint {
+
+	/**
+	 * Should be provided in the {@link #getPluginParams()} method
+	 * as a key parameter key. Value is supported commands by plugin
+	 */
+	String ALLOWED_COMMANDS = "allowedCommands";
 
 	/**
 	 * Return available plugin parameters
