9c0cf3a80b4be6266abc981a0880414f5e056c65
Pavel_Bortnik <pavel_bortnik@epam.com>
2021-12-04 20:00:34 +0300

    EPMRPP-68947 || Refactor interfaces

--- src/main/java/com/epam/reportportal/extension/CommonPluginCommand.java
+++ src/main/java/com/epam/reportportal/extension/CommonPluginCommand.java
@@ -5,7 +5,7 @@ import java.util.Map;
 /**
  * @author <a href="mailto:pavel_bortnik@epam.com">Pavel Bortnik</a>
  */
-public interface CommonPluginCommand<T> extends NamedPluginCommand<T> {
+public interface CommonPluginCommand<T> {
 
 	/**
 	 * Executes plugin command without existed integration
