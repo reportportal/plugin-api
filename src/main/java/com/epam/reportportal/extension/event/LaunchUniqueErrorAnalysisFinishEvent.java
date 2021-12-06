ecb5ac87da001a4d392c2723ad023abdd5db63d2
Ivan_Budayeu <Skileton2018>
2021-12-06 13:33:39 +0300

    EPMRPP-57215 || Start unique error analysis event added

--- src/main/java/com/epam/reportportal/extension/event/LaunchUniqueErrorAnalysisFinishEvent.java
+++ src/main/java/com/epam/reportportal/extension/event/LaunchUniqueErrorAnalysisFinishEvent.java
@@ -1,20 +1,12 @@
 package com.epam.reportportal.extension.event;
 
-import java.util.Map;
-
 /**
  * @author <a href="mailto:ivan_budayeu@epam.com">Ivan Budayeu</a>
  */
 public class LaunchUniqueErrorAnalysisFinishEvent extends LaunchEvent<Long> {
 
-	private final Map<String, Object> metadata;
-
-	public LaunchUniqueErrorAnalysisFinishEvent(Long source, Map<String, Object> metadata) {
-		super(source);
-		this.metadata = metadata;
+	public LaunchUniqueErrorAnalysisFinishEvent(Long id) {
+		super(id);
 	}
 
-	public Map<String, Object> getMetadata() {
-		return metadata;
-	}
 }
