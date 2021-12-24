886ac552765f90c1348476f0067741ba402304f4
Pavel Bortnik <pavel_bortnik@epam.com>
2021-12-24 12:02:24 +0300

    EPMRPP-68947 || Information about Jira Cloud issue (#39)
    * EPMRPP-68947 || Introduce common plugin commands without existed integration
    
    * EPMRPP-68947 || Rename plugin commands
    
    * EPMRPP-68947 || Refactor interfaces
    
    * EPMRPP-68947 || Refactor interfaces
    
    * EPMRPP-68947 || Refactor interfaces
    
    * EPMRPP-68947 || Add common commands field

--- src/main/java/com/epam/reportportal/extension/PluginCommand.java
+++ src/main/java/com/epam/reportportal/extension/PluginCommand.java
@@ -7,10 +7,10 @@ import java.util.Map;
 /**
  * @author <a href="mailto:pavel_bortnik@epam.com">Pavel Bortnik</a>
  */
-public interface PluginCommand<T> {
+public interface PluginCommand<T> extends NamedPluginCommand {
 
 	/**
-	 * Executes plugin command
+	 * Executes plugin command for existed integration
 	 *
 	 * @param integration Configured ReportPortal integration
 	 * @param params      Plugin Command parameters
