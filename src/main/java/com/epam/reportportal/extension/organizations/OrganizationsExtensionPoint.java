29e3daa16b77470bd3c86b21aa21b3895122f1b3
siarhei_hrabko <siarhei_hrabko@epam.com>
2024-12-13 12:49:04 +0300

    org-poc

--- src/main/java/com/epam/reportportal/extension/organizations/OrganizationsExtensionPoint.java
+++ src/main/java/com/epam/reportportal/extension/organizations/OrganizationsExtensionPoint.java
@@ -1,15 +1,13 @@
 package com.epam.reportportal.extension.organizations;
 
 import com.epam.ta.reportportal.commons.ReportPortalUser;
-import com.epam.ta.reportportal.ws.model.EntryCreatedRS;
 import org.pf4j.ExtensionPoint;
 
 public interface OrganizationsExtensionPoint extends ExtensionPoint {
 
-  EntryCreatedRS createOrganization(String orgName, ReportPortalUser user);
+  Object createOrganization(String orgName, ReportPortalUser user);
 
-  EntryCreatedRS updateOrganization(String id, String orgName, ReportPortalUser user);
 
-  EntryCreatedRS deleteOrganization(String id, ReportPortalUser user);
+  void updateOrganization(String orgName, ReportPortalUser user);
 
 }
