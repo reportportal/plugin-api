050a1fda2b5a8aafa243c1ccedf19321a3ccbeed
siarhei_hrabko <siarhei_hrabko@epam.com>
2025-02-06 12:26:13 +0300

    EPMRPP-99152 revert ExtensionContainer implementation

--- src/main/java/com/epam/reportportal/extension/lookup/ExtensionContainer.java
+++ src/main/java/com/epam/reportportal/extension/lookup/ExtensionContainer.java
@@ -1,29 +1,29 @@
 package com.epam.reportportal.extension.lookup;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @Service
-@Deprecated(forRemoval = true)
-public class ExtensionContainer<T> {
+public class ExtensionContainer {
 
-	private final Map<String, T> extensions;
+  private final Map<String, Object> extensions;
 
-	public ExtensionContainer() {
-		this.extensions = new LinkedHashMap<>();
-	}
+  public ExtensionContainer() {
+    this.extensions = new LinkedHashMap<>();
+  }
 
-	public void add(String key, T extension) {
-		extensions.put(key, extension);
-	}
+  public void add(String key, Object extension) {
+    extensions.put(key, extension);
+  }
 
   public void remove(String key) {
-		extensions.remove(key);
-	}
+    extensions.remove(key);
+  }
 
-	public T get(String key) {
-		return extensions.get(key);
-	}
+  public <T> T get(String key) {
+    return (T) extensions.get(key);
+  }
 
 }
